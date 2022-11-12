62e1eb1d67cf50d24d1e5ebc46ef95b30c4f7316
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2022-11-12 00:20:03 -0800

    tested sql queries

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -4,7 +4,7 @@ from flask_app.models import user, item, category
 from flask_app.controllers import items
 from flask import session
 
-@app.route('/register/', methods=["POST"])
+@app.route('/register', methods=["POST"])
 def register_user():
     if user.User.create_user(request.form):
         return redirect ('/dashboard')
--- flask_app/models/item.py
+++ flask_app/models/item.py
@@ -1,10 +1,10 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
-# from flask_app.models import user
+from flask_app.models import user
 from flask_app.models import category
 
 class Item:
-    db = 'SecondHandSourcing'
+    db = 'SecondHandSourcingSchema'
     def __init__(self , data):
         self.id = data['id']
         self.item_name = data['item_name']
@@ -16,7 +16,7 @@ class Item:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
         self.user_id = data['user_id']
-        self.category_id = data ['categories_id']
+        self.category_id = data ['category_id']
         self.user = None
         self.category = None
 
@@ -33,12 +33,13 @@ class Item:
         return items
 
     @classmethod
-    def get_one(cls, data):
+    def get_one(cls, id):
+        data = {'id': id}
         query = """
-            SELECT *
-            FROM items
-            WHERE id = %(id)s;
-            """
+        SELECT *
+        FROM items
+        WHERE id = %(id)s;
+        ;"""
         results = connectToMySQL(cls.db).query_db(query, data)
         if len(results) < 1:
             return False
@@ -47,56 +48,30 @@ class Item:
     @classmethod
     def save(cls, data):
         query = """
-            INSERT INTO items (item_name, cost, location, image, breif_desc, details, user_id, categories_id)
-            VALUES (%(item_name)s, %(cost)s, %(location)s, %(image)s, %(breif_desc)s, %(details)s, %(user_id)s, %(categories_id)s);
+            INSERT INTO items (item_name, cost, location, image, brief_desc, details, user_id, category_id)
+            VALUES (%(item_name)s, %(cost)s, %(location)s, %(image)s, %(brief_desc)s, %(details)s, %(user_id)s, %(category_id)s);
             """
         return connectToMySQL(cls.db).query_db(query, data)
 
     @classmethod
     def update(cls, data):
         query = """
-            UPDATE items 
-            SET item_name = %(item_name)s, cost = %(cost)s, location = %(location)s, image = %(image)s, breif_desc = %(breif_desc)s, details = %(details)s WHERE id = %(id)s;
-        """
+        UPDATE items 
+        SET item_name = %(item_name)s, cost = %(cost)s, location = %(location)s, image = %(image)s, brief_desc = %(brief_desc)s, details = %(details)s 
+        WHERE id = %(id)s
+        ;"""
         print('update', data)
         return connectToMySQL(cls.db).query_db(query, data)
 
     @classmethod
-    def delete(cls, data):
+    def delete(cls, id):
+        data = { 'id':id} 
         query = """
             DELETE FROM items
             WHERE id = %(id)s;
         """
         return connectToMySQL(cls.db).query_db(query, data)
 
-    @classmethod
-    def get_user_items_by_user_id(cls, data):
-        query = """
-            SELECT * 
-            FROM users
-            LEFT JOIN items
-            ON users.id = items.user_id
-            WHERE id = %(id)s;
-            """
-        result = connectToMySQL(cls.db).query_db(query, data)
-        user_with_items = cls(result[0])
-        for each_item in result:
-            item_data = {
-                'id' : each_item['item.id'],
-                'item_name' : each_item['item_name'],
-                'cost' : each_item['cost'],
-                'location' : each_item['location'],
-                'image' : each_item['image'],
-                'breif_desc' : each_item['breif_desc'],
-                'details' : each_item['details'],
-                'created_at' : each_item['created_at'],
-                'updated_at' : each_item['updated_at'],
-                'user_id' : each_item['user_id'],
-                'category_id' : each_item['categories_id']
-        }
-        single_item = Item(item_data)
-        user_with_items.items.append(single_item)
-        return user_with_items
 
     @staticmethod
     def validate(item):
@@ -107,7 +82,7 @@ class Item:
         if len(item['location']) < 2:
             isValid = False
             flash ('Please enter a location with at least 2 Characters!')
-        if len(item['breif_desc']) > 25:
+        if len(item['brief_desc']) > 25:
             isValid = False
             flash ('Description can be a maximum of 25 characters!')
         if len(item['details']) < 2:
